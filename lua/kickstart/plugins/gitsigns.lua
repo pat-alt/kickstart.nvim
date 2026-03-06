ef6cf8e7a9ca0b4bc7a0e9f71f7d6d1f57346de5
Patrick Altmeyer <paltmeyer@hbk.com>
2026-03-06 10:56:32 +0100

    increase visibility of git changes

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -3,15 +3,16 @@ return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     tag = 'v0.7',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
+    config = function()
+      require('gitsigns').setup {
+        signs = {
+          add = { text = '│' },
+          change = { text = '│' },
+          delete = { text = '▸' },
+          topdelete = { text = '▾' },
+          changedelete = { text = '│' },
+        },
+        on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
         local function map(mode, l, r, opts)
@@ -61,7 +62,15 @@ return {
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
         map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
       end,
-    },
+      }
+
+      -- Explicit highlight colors for git signs
+      vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = '#22c55e' })
+      vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#eab308' })
+      vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#ef4444' })
+      vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { fg = '#ef4444' })
+      vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { fg = '#ef4444' })
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
